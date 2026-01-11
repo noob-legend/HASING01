88c51a415bd966a89cecb9d9e18f320d74d5e279
noob-legend <legendone821@gmail.com>
2026-01-11 18:54:14 +0700

    resolve conflict

--- server/app.js
+++ server/app.js
@@ -11,6 +11,10 @@ connectDB();
 app.use(express.json());
 app.use("/api/users", userRoutes);
 
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
